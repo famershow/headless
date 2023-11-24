9482c11e4ec3359d92c538c27e088918e85ce230
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2023-11-23 20:19:37 -0500

    Fix flickering
    Fix flickering
    
    Move patch to theme
    
    Update dependencies and remove patch
    
    Patched v1.1.0

--- templates/hydrogen-theme/app/hooks/useSanityClient.tsx
+++ templates/hydrogen-theme/app/hooks/useSanityClient.tsx
@@ -1,16 +1,21 @@
 import { getSanityClient } from "~/lib/sanity/client";
 import { useEnvironmentVariables } from "./useEnvironmentVariables";
+import { useMemo } from "react";
 
 export function useSanityClient() {
   const env = useEnvironmentVariables();
-  const { client } = getSanityClient({
-    projectId: env?.SANITY_STUDIO_PROJECT_ID!,
-    dataset: env?.SANITY_STUDIO_DATASET!,
-    studioUrl: env?.SANITY_STUDIO_URL!,
-    useStega: env?.SANITY_STUDIO_USE_STEGA!,
-    apiVersion: env?.SANITY_STUDIO_API_VERSION!,
-    useCdn: env?.NODE_ENV === "production",
-  });
+  const { client } = useMemo(
+    () =>
+      getSanityClient({
+        projectId: env?.SANITY_STUDIO_PROJECT_ID!,
+        dataset: env?.SANITY_STUDIO_DATASET!,
+        studioUrl: env?.SANITY_STUDIO_URL!,
+        useStega: env?.SANITY_STUDIO_USE_STEGA!,
+        apiVersion: env?.SANITY_STUDIO_API_VERSION!,
+        useCdn: env?.NODE_ENV === "production",
+      }),
+    [env]
+  );
 
   return client;
 }
