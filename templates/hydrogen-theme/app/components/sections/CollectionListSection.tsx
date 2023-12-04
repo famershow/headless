8723db3476696b833e77fdd36fb0d2a7c8d6fb41
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2023-12-04 12:22:59 -0500

    Use Promise.allSettled in resolveShopifyPromises

--- templates/hydrogen-theme/app/components/sections/CollectionListSection.tsx
+++ templates/hydrogen-theme/app/components/sections/CollectionListSection.tsx
@@ -1,4 +1,5 @@
 import type {TypeFromSelection} from 'groqd';
+import type {CollectionsQuery} from 'storefrontapi.generated';
 
 import {Await, useLoaderData} from '@remix-run/react';
 import {Suspense} from 'react';
@@ -14,37 +15,67 @@ type CollectionListSectionProps = TypeFromSelection<
   typeof COLLECTION_LIST_SECTION_FRAGMENT
 >;
 
+/**
+ * `CollectionListSection` is a section that displays a list of collections.
+ * The collections data is fetched from Shopify using the `collectionListPromise`
+ * returned by the loader. The data is streamed to the client so we need to use a `Suspense`
+ * component and to display a `Skeleton` while waiting for the data to be available.
+ */
 export function CollectionListSection(
   props: SectionDefaultProps & {data: CollectionListSectionProps},
 ) {
+  // Todo => Add a skeleton
+  return (
+    <AwaitCollectionList
+      fallback={<div className="container">Loading...</div>}
+      sanityData={props.data}
+    >
+      {(collections) => (
+        <div className="container">
+          <CollectionListGrid
+            collections={collections}
+            columns={props.data.desktopColumns}
+          />
+        </div>
+      )}
+    </AwaitCollectionList>
+  );
+}
+
+function AwaitCollectionList(props: {
+  children: (collections: CollectionsQuery['collections']) => React.ReactNode;
+  fallback: React.ReactNode;
+  sanityData: CollectionListSectionProps;
+}) {
   const loaderData = useLoaderData<typeof indexLoader>();
   const collectionListPromise = loaderData?.collectionListPromise;
-  const sectionGids = props.data.collections
+  const sanityCollectionListGids = props.sanityData.collections
     ?.map((collection) => collection.store.gid)
     .join(',');
 
-  return collectionListPromise ? (
-    <Suspense fallback={<div className="container">Loading...</div>}>
+  return (
+    <Suspense fallback={props.fallback}>
       <Await resolve={collectionListPromise}>
         {(data) => {
           // Resolve the collection list data from Shopify with the gids from Sanity
-          const collections = data.find(({collections}) => {
-            const gids = collections.nodes
-              .map((collection) => collection.id)
-              .join(',');
-            return sectionGids === gids ? collections : null;
-          })?.collections;
+          const collections = data.map((result) => {
+            // Check if the promise is fulfilled
+            if (result.status === 'fulfilled') {
+              const {collections} = result.value;
+              const shopifyCollectionListGids = collections.nodes
+                .map((collection) => collection.id)
+                .join(',');
+              // Compare the Sanity gids with the Shopify gids
+              return sanityCollectionListGids === shopifyCollectionListGids
+                ? collections
+                : null;
+            }
+            return null;
+          })[0];
 
-          return (
-            <div className="container">
-              <CollectionListGrid
-                collections={collections}
-                columns={props.data.desktopColumns}
-              />
-            </div>
-          );
+          return <>{collections && props.children(collections)}</>;
         }}
       </Await>
     </Suspense>
-  ) : null;
+  );
 }
--- templates/hydrogen-theme/app/lib/sanity/sanity.payload.server.ts
+++ templates/hydrogen-theme/app/lib/sanity/sanity.payload.server.ts
@@ -9,7 +9,7 @@ type SanityPayload = {
 /**
  * The `sanityPreviewPayload` object is used by the `useSanityData` hook.
  * It is used to pass the query and params to the Sanity client and fetch live data from Sanity Studio.
- * The payload will be returned as `null` if the `sanityPreviewMode` is false.
+ * The payload will be returned as `null` if `sanityPreviewMode` is set to false.
  **/
 export function sanityPreviewPayload({context, params, query}: SanityPayload) {
   const {sanityPreviewMode} = context;
