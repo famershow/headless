95772531d9a1b1ab2083626d6ed9e1ea25f29732
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2023-12-07 18:15:22 -0500

    Update cart with optimistic inputs
    Update

--- templates/hydrogen-theme/app/routes/($locale).cart.tsx
+++ templates/hydrogen-theme/app/routes/($locale).cart.tsx
@@ -4,14 +4,12 @@ import type {
   LoaderFunctionArgs,
 } from '@shopify/remix-oxygen';
 
-import {Link, useLoaderData} from '@remix-run/react';
-import {CartForm, Image, Money, flattenConnection} from '@shopify/hydrogen';
+import {useLoaderData} from '@remix-run/react';
+import {CartForm} from '@shopify/hydrogen';
 import {defer, json} from '@shopify/remix-oxygen';
-import {cx} from 'class-variance-authority';
-import {Link as ButtonLink} from 'react-aria-components';
 import invariant from 'tiny-invariant';
 
-import {useLocalePath} from '~/hooks/useLocalePath';
+import {Cart} from '~/components/Cart';
 import {isLocalPath} from '~/lib/utils';
 
 export async function action({context, request}: ActionFunctionArgs) {
@@ -95,66 +93,6 @@ export async function loader({context}: LoaderFunctionArgs) {
 
 export default function CartRoute() {
   const {cart} = useLoaderData<typeof loader>();
-  const padding = cx('py-20');
-  const lines = flattenConnection(cart?.lines);
 
-  return (
-    <div className={cx(['container', padding])}>
-      <div className="flex justify-between gap-5">
-        <ul className="grid gap-4">
-          {lines.map((line) => (
-            <LineItem key={line.id} line={line} />
-          ))}
-        </ul>
-        <div>
-          <div className="flex items-center gap-2">
-            <span>Total</span>
-            {cart?.cost.totalAmount && <Money data={cart.cost.totalAmount} />}
-          </div>
-          <div className="mt-4">
-            <ButtonLink
-              className="inverted-color-scheme"
-              href={cart?.checkoutUrl}
-            >
-              {/* Todo => get text from sanity */}
-              Proceed to checkout
-            </ButtonLink>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export function LineItem(props: {
-  line: CartQueryData['cart']['lines']['nodes'][0];
-}) {
-  const {line} = props;
-  const productPath = useLocalePath({
-    path: `/products/${line.merchandise.product.handle}`,
-  });
-
-  return (
-    <li key={line.id}>
-      <div className="flex items-center gap-4">
-        {line.merchandise.image && (
-          <div className="aspect-square h-auto w-10">
-            <Link prefetch="intent" to={productPath}>
-              <Image
-                className="h-full w-full rounded object-cover"
-                data={line.merchandise.image}
-                sizes="40px"
-              />
-            </Link>
-          </div>
-        )}
-        <div className="flex flex-col gap-1">
-          <span>
-            {line.merchandise.product.title} x {line.quantity}
-          </span>
-          <span className="text-sm opacity-80">{line.merchandise.title}</span>
-        </div>
-      </div>
-    </li>
-  );
+  return <Cart cart={cart} layout="page" />;
 }
